d35bc3a623aec417ebce318d360e7821d443d44e
unknown <da.fan@xinboxinmo.com>
2018-10-17 17:25:16 +0800

    backfanda

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="hello1">
     <h1 @click="getInfo">{{ msg }}</h1>
     <el-button type="primary" @click="turnSku">主要按钮</el-button>
   </div>
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HelloWorld1',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -16,6 +16,7 @@ export default {
   methods:{
     getInfo(){
       this.$http.get(this.URL.Get_book_info).then(data=>{
+        
         console.log(data)
       })
     },
